52a9a85ce406ff6ba13db4dd973c69fa20449433
mehuljain2003 <85852792+mehuljain2003@users.noreply.github.com>
2025-05-07 09:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "29907731"))
+API_HASH = getenv("API_HASH", "8f59d632cb374705cfdee46ac17cc3cd")
+BOT_TOKEN = getenv("BOT_TOKEN", "7556497777:AAF4VoCDNvHvQ_DiAfpT9uP703UFzClvcgY")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "1000"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
